8e1b0697b44ad147dca109e0dffd0742c9e09d28
Your Name <natownley@gmail.com>
2019-08-12 22:58:50 -0500

    coding before debugging

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -11,7 +11,7 @@ $(document).ready (function() {
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  var database = firebase.database();
+  var newDatabase = firebase.database();
   
   $("#newTrain").on("click", function (event) {
       event.preventDefault();
@@ -21,7 +21,7 @@ $(document).ready (function() {
     firstTrain = $("#fttime1").val().trim();
     frequence = $("#freq1").val().trim();
 
-    database.ref().push({
+    newDatabase.ref().push({
       trainName:trainName,
       destination:destiny,
       firstTrain:firstTrain,
@@ -29,5 +29,28 @@ $(document).ready (function() {
     });
 
   });
-  database.ref
-})
+  datebase.ref().on("child_added", function*(childSnapshot) {
+
+var nTrain = childSnapshot.val().trainName;
+var nLocation = childSnapshot.val().destination;
+var nFirstTrain = childSnapshot.val().firstTrain;
+var nFreq = childSnapshot.val().frequency;
+
+var convertStartTime = moment(nFirstTrain, "hh:mm").subtract (1,"years");
+var currentTime= moment ();
+var diffTime = moment().diff(moment(convertStartTime), "minutes");
+var tRemainder = diffTime % nFreq;
+var tMinutesTillTrain = nFreq-tRemainder;
+var nXtrain = moment().add(tMinutesTillTrain, "minutes");
+var trainComes = moment (nTrain).format("HH:mm");
+
+$("#trainDisplay").append(
+'<tr><td>' + nTrain + 
+'</td><td>' + nLocation +
+'</td><td>' + nFreq +
+'</td><td>' + trainComes +
+'</td><td>' + tMinutesTillTrain + '</td></tr>');
+
+
+    })
+    })
